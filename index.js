fbc3b37c724566354bf30c32f7a35c00b87560c9
Rmaki27 <engineermaki27@gmail.com>
2023-03-25 12:08:45 -0700

    Implement for of to access cat powers

---
+++ index.js
@@ -0,0 +1,31 @@
+const characters = [
+    {
+        title: 'Ninja',
+        emoji: '🥷',
+        powers: ['agility', 'stealth', 'aggression'],
+    },
+    {
+        title: 'Sorcerer',
+        emoji: '🧙',
+        powers: ['magic', 'invisibility', 'necromancy'],
+    },
+    {
+        title: 'Ogre',
+        emoji: '👹',
+        powers: ['power', 'stamina', 'shapeshifting'],
+    },
+    {
+        title: 'Unicorn',
+        emoji: '🦄',
+        powers: ['flight', 'power', 'purity'],
+    }
+]
+
+for (let character of characters) {
+    console.log(character.powers)
+    for (let power of character.powers) {
+        console.log(power)
+    }
+}
+
+
